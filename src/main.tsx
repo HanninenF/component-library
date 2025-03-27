9916a4d368cc55d4ec862296f478e2b68f0d4ae4
HanninenF <hanninenfredrik@gmail.com>
2025-03-27 15:54:13 +0100

    Update main entry file
    Adjusted React rendering setup or routing structure in main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
-import App from "./App.tsx";
+/* import App from "./App.tsx";
 import App2 from "./App2.tsx";
 import App3 from "./App3.tsx";
 import App4 from "./App4.tsx";
+import App5 from "./App5.tsx"; */
+import App6 from "./App6";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <App6 />
     {/*   <App2 /> */}
     {/* <App3 /> */}
-    <App4 />
+    {/*  <App4 /> */}
   </StrictMode>
 );
